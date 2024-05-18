8b940300f8e61ace33cb457cb67b2aff84738e82
byxmend <pestraknv@yandex.ru>
2024-05-18 07:29:01 +0300

    init

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ with open('data/addresses.txt', 'r', encoding='utf-8-sig') as file:
 with open('data/sybil_wallets.txt', 'r', encoding='utf-8-sig') as file:
     sybil_wallets = {line.strip() for line in file}
 
-with open('data/result_sybil_wallets.txt', 'w', encoding='utf-8-sig') as result_file:
+with open('data/result.txt', 'w', encoding='utf-8-sig') as result_file:
     for address in addresses:
         if address in sybil_wallets:
             result_file.write(address + '\n')
